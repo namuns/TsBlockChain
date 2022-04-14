fef5a32a892508b47405fd894f3b0b41b2d39537
namuns <skymsn214@gmail.com>
2022-04-15 02:47:40 +0900

    ts를 사용한 block 생성

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,68 @@
-class Human {
-    public name: string;
-    public age: number;
-    public gender: string;
-    constructor(name: string, age:number, gender:string){
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
-    } 
+import * as CryptoJS from "crypto-js";
+
+class Block {
+    public index:number;
+    public hash: string;
+    public previousHash: string;
+    public data: string;
+    public timestamp: number; 
+
+    static calculateBlockHash = (
+        index:number, 
+        previousHash:string, 
+        timestamp:number, 
+        data:string
+        ): string => 
+        CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+    
+    constructor(
+        index:number,
+        hash: string,
+        previousHash: string,
+        data: string,
+        timestamp: number
+        ){
+            this.index = index;
+            this.hash = hash;
+            this.previousHash = previousHash;
+            this.data = data;
+            this.timestamp = timestamp;
+        }
 }
 
-const person = {
-    name: "moons",
-    age: 22,
-    gender: "male"
+const genesisBlock:Block = new Block(0, "210229192", "", "Hello", 123456);
 
-}
+let blockchain: Block[] = [genesisBlock];
 
-const sayHi = (person: Human): string => {
-    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`; 
-};
-console.log(sayHi(person));
+const getBlockchain = () : Block[] => blockchain;
 
-export {}
+const getLatestBlock = () : Block => blockchain[blockchain.length - 1];
+
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
+
+
+const createNewBlock = (data: string): Block => {
+    const previosBlock: Block = getLatestBlock();
+    const newIndex: number = previosBlock.index + 1;
+    const newTimestamp: number = getNewTimeStamp();
+    const newHash: string = Block.calculateBlockHash(
+      newIndex,
+      previosBlock.hash,
+      newTimestamp,
+      data
+    );
+    const newBlock: Block = new Block(
+      newIndex,
+      newHash,
+      previosBlock.hash,
+      data,
+      newTimestamp
+    );
+    return newBlock;
+  };
+
+  console.log(createNewBlock("hello"));
+  console.log(createNewBlock("bye bye"));
+
+
+export {};
