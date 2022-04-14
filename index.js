de3914652a6c5fe63800bf3dbc14cf0b6961546d
namuns <skymsn214@gmail.com>
2022-04-15 01:48:45 +0900

    tsc watch로 yarn start 그만하고 string설정시 log적용

--- index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const name = "Namuns", age = 25, gender = "male";
-const sayHi = (name, age, gender) => {
-    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
-};
-sayHi(name, age, gender);
-//# sourceMappingURL=index.js.map
