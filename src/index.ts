4c7e3697b045c9aa09a8bb30d67aa145680d2cfa
namuns <skymsn214@gmail.com>
2022-04-15 02:01:01 +0900

    learn to ts class와 public/private

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
-interface Human {
-    name: string;
-    age: number;
-    gender: string;
+class Human {
+    public name: string;
+    public age: number;
+    public gender: string;
+    constructor(name: string, age:number, gender:string){
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    } 
 }
 
 const person = {
